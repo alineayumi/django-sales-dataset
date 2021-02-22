417a9910e08c508813868f2da0455ae758428754
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-02-22 20:22:52 -0300

    feat: implementing base template with navbar

--- sales_project/sales_app/views.py
+++ sales_project/sales_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, View,
+                                  ListView, DetailView,
+                                  CreateView, UpdateView,
+                                  DeleteView)
+from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
--- sales_project/sales_project/settings.py
+++ sales_project/sales_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = Path.joinpath(BASE_DIR, "templates")
+STATIC_DIR = Path.joinpath(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'sales_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR]
