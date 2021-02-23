131d1ba159bf4e34c5f24ce9fe4bf11a682de20a
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-02-23 11:45:33 -0300

    feat: basic layout to product list page

--- sales_project/sales_app/urls.py
+++ sales_project/sales_app/urls.py
@@ -4,5 +4,6 @@ from sales_app import views
 app_name = 'sales_app'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='products')
 ]
